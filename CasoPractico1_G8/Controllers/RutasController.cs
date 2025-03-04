141aa7c4c184f6eb4a47468ac74c613f680902e2
danism27 <danielsm27@hotmail.com>
2025-03-04 03:25:45 -0600

    Update RutasController.cs

--- CasoPractico1_G8/Controllers/RutasController.cs
+++ CasoPractico1_G8/Controllers/RutasController.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CasoPractico1_G8.Models;
-using System.Security.Claims;
+
 
 namespace CasoPractico1_G8.Controllers
 {
